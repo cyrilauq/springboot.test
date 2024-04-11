33fc19e359c90dee8c9a107e8c9dc0b3a5757388
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-04-11 09:32:56 +0200

    first commit

---
+++ src/main/java/com/example/demo/controllers/TestController.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/SpringFramework/RestController.java to edit this template
+ */
+package com.example.demo.controllers;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.util.ArrayList;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+/**
+ *
+ * @author cyril
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    
+    @GetMapping()
+    public List<String> list() {
+        List<String> result = new ArrayList();
+        result.add("one");
+        result.add("test");
+        result.add("is");
+        result.add("good");
+        return result;
+    }
+    
+    @GetMapping("/{id}")
+    @ApiResponse(responseCode = "200", description = "Success get")
+    @ApiResponse(responseCode = "400", description = "Bad id")
+    public Object get(@PathVariable String id) {
+        return null;
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<?> put(@PathVariable String id, @RequestBody Object input) {
+        return null;
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> post(@RequestBody Object input) {
+        return null;
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable String id) {
+        return null;
+    }
+    
+}
